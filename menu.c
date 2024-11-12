0f50b9ad39ca2184085d86bc5640d19c3bd4df12
ronerog <ronerojr@gmail.com>
2024-11-12 15:50:30 -0300

    [feat]: list quantity orders

--- menu.c
+++ menu.c
@@ -9,12 +9,15 @@ void menu() {
     float total = 0;
     char listapedido[50][50];
     int length = 0;
+    int quant = 0;
+    char pedido[50];
+    int confirma;
     setlocale(LC_ALL, "");
     clear_screen();
 
     int opcao;
 
-
+        do {
         printf("            Menu            \n");
         printf("1- Hamburguer: R$ 25,00\n");
         printf("2- Pizza: R$ 39,00\n");
@@ -22,8 +25,8 @@ void menu() {
         printf("4- Onion Rings: R$ 12,00\n");
         printf("0- Finalizar pedido\n");
 
-        do {
-        printf("Escolha uma opcao: ");
+
+        printf("Selecione um item ou aperte 0 para finalizar o pedido: \n");
         if (scanf("%d", &opcao) != 1) {
             printf("Entrada invalida! Tente novamente.\n");
             return;
@@ -32,42 +35,61 @@ void menu() {
         switch (opcao) {
             case 1:
                 printf("Hamburguer selecionado.\n");
-                printf("Selecione outro item ou aperte 0 para finalizar o pedido.\n");
-                total += 25.00;
-                strcpy(listapedido[length], "1- Hamburguer: R$ 25,00");
+                printf("Selecione a quantidade.\n");
+                scanf("%i", &quant);
+                total += 25.00 * quant;
+                sprintf(pedido, "%i - Hamburguer: R$ %.2f", quant, quant * 25.00);
+                strcpy(listapedido[length], pedido);
                 length++;
                 break;
             case 2:
                 printf("Pizza selecionada.\n");
-                printf("Selecione outro item ou aperte 0 para finalizar o pedido.\n");
-                total += 39.00;
-                strcpy(listapedido[length], "2- Pizza: R$ 39,00");
+                printf("Selecione a quantidade\n");
+                scanf("%i", &quant);
+                total += 39.00 * quant;
+                sprintf(pedido, "%i - Pizza: R$ %.2f", quant, quant * 39.00);
+                strcpy(listapedido[length], pedido);
                 length++;
                 break;
             case 3:
                 printf("Batata Frita selecionada.\n");
-                printf("Selecione outro item ou aperte 0 para finalizar o pedido.\n");
-                total += 10.00;
-                strcpy(listapedido[length], "3- Batata Frita: R$ 10,00");
+                printf("Selecione a quantidade\n");
+                scanf("%i", &quant);
+                total += 10.00 * quant;
+                sprintf(pedido, "%i - Batata Frita: R$ %.2f", quant, quant * 10.00);
+                strcpy(listapedido[length], pedido);
                 length++;
                 break;
             case 4:
                 printf("Onion Rings selecionado.\n");
-                printf("Selecione outro item ou aperte 0 para finalizar o pedido.\n");
-                total += 12.00;
-                strcpy(listapedido[length], "4- Onion Rings: R$ 12,00");
+                printf("Selecione a quantidade\n");
+                scanf("%i", &quant);
+                total += 12.00 * quant;
+                sprintf(pedido, "%i - Onion Rings: R$ %.2f", quant, quant * 12.00);
+                strcpy(listapedido[length], pedido);
                 length++;
                 break;
             case 0:
-                printf("Finalizando pedido...\n");
                 printf("Itens do pedido:\n");
                 for (int i = 0; i < length; i++) {
                     printf("%s\n", listapedido[i]);
                 }
                 printf("Total: R$ %.2f\n", total);
+
+                printf("Confirmar pedido?\n");
+                printf("1- Sim\n");
+                printf("2- Nao\n");
+                scanf("%i", &confirma);
+                if(confirma == 1) {
+                printf("Pedido realizado com sucesso.\n");
+                printf("Bom apetite!\n");
+                } else if(confirma == 2) {
+                printf("Pedido cancelado.\n");
+                } else {
+                printf("Opcao invalida! Tente novamente.\n");
+                }
                 return;
             default:
-                printf("Opcao invalida! Tente novamente.\n");
                 break;
         }
     } while (opcao != 0);
